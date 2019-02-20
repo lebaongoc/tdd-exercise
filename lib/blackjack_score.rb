805435b9a0fc6de1247f952dc1f0110198fc37fc
Ngoc Le <ngocle@outlook.com>
2019-02-20 13:55:05 -0800

    blackjack_score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,10 @@
 # blackjack_score.rb
+# link: https://github.com/lebaongoc/tdd-exercise
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
 
 def blackjack_score(hand)
-
+  hand.map! { |card| card == "King" || card == "Queen" || card == "Jack" ? 10 : card }
+  score = hand.sum
+  return score
 end
